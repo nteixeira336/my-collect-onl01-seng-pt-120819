af4c2893f553ed3e3589a03c6f24684302ef8c66
Ninibeth Teixeira <ninibethreina@gmail.com>
2019-12-20 15:19:07 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
+def my_collect(languages)
+  i=0
+  capitzlized_languages= [ ]
+  while i<languages.length  
+  capitzlized_languages<< yield(languages[i])
+  i+=1 
+end 
+capitzlized_languages
+end 
 
