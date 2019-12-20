f0d8d4006b182c27c91cc07362422a2024537729
Ninibeth Teixeira <ninibethreina@gmail.com>
2019-12-20 15:15:58 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,10 @@
-def my_collect(array)
+def my_collect(languages)
   i=0
-  my_collect= [ ]
-  while i<array.length 
-  my_collect<< yield(array[i])
+  capitzlized_languages= [ ]
+  while i<languages.length 
+  capitzlized_languages<< yield(languages[i])
   i+=1 
 end 
+capitzlized_languages
 end 
 
